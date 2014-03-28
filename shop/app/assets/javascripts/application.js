9c1e7fb24897ab7f1692eb0cbec3eac52b2ced6d
Arthur Yii <841158840@qq.com>
2014-03-28 16:02:16 +0800

    2014-03-28

--- shop/app/assets/javascripts/application.js
+++ shop/app/assets/javascripts/application.js
@@ -1,16 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+$(function () {
+    var header = new Headroom(document.querySelector("#header"), {
+        tolerance: 5,
+        offset: 205,
+        classes: {
+            initial: "animated",
+            pinned: "flipInX",
+            unpinned: "flipOutX"
+        }
+    });
+    header.init();
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '', // Text for element
+        activeOverlay: false  // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+});
